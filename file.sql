84d2ad221b947a7f1794c9adcd831ee3e1cddbca
nafa39 <nafatulmiskiyah@gmail.com>
2024-11-20 13:31:09 +0700

    insert data each table

--- file.sql
+++ file.sql
@@ -46,3 +46,45 @@ CREATE TABLE IF NOT EXISTS TransactionsDetails (
 	FOREIGN KEY (TransactionId) REFERENCES Transactions(id),
 	FOREIGN KEY (ProductId) REFERENCES Products(id)
 );
+
+-- insert data user
+INSERT INTO UserAdmin (Email, Password) VALUES
+('nafa@gmail.com', 'admin1'),
+('yugo@gmail.com', 'admin2'),
+('ferdinand@gmail.com', 'admin3');
+
+-- insert data Customers
+INSERT INTO Customers (Name, Address, Email, PhoneNumber) VALUES
+('Alice Johnson', 'Bandung', 'alice.johnson@gmail.com', '+6212345678'),
+('Bob Smith', 'Yogyakarta', 'bob.smith@example.com', '+6223458901'),
+('Clara Davis', 'Surabaya', 'clara.davis@example.com', '+6256789012'),
+('David Brown', 'Jakarta', 'david.brown@example.com', '+6245670123'),
+('Eva White', 'Medan', 'eva.white@example.com', '+6256781234');
+
+-- insert data Products
+INSERT INTO Products (ProductName, Price, Stock) VALUES
+('Kemeja Polo', 50000.00, 10),
+('Daster Ibu Hamil', 40000.00, 20),
+('Topi Anak', 25000.00, 100),
+('Kaos Kaki Budak Korporat', 60000.00, 15),
+('Dasi Bos', 2000000.00, 25);
+
+-- insert data Transactions
+INSERT INTO Transactions (CustomerId, Date) VALUES
+(1, '2024-11-01'),
+(2, '2024-11-02'),
+(3, '2024-11-03'),
+(4, '2024-11-04'),
+(5, '2024-11-05');
+
+-- insert data transaction details
+INSERT INTO TransactionsDetails (TransactionId, ProductId, Quantity, TotalPrice) VALUES
+(1, 1, 1, 50000.00), 
+(1, 3, 2, 50000.00),  
+(2, 2, 1, 40000.00),
+(3, 4, 1, 60000.00),  
+(3, 3, 1, 25000.00),   
+(4, 5, 2, 4000000.00), 
+(5, 1, 1, 50000.00), 
+(5, 2, 1, 40000.00),  
+(5, 3, 3, 75000.00);  
