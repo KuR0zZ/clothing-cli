59f175670d30d71c216028573e2a5cbe370a0ce9
nafa39 <nafatulmiskiyah@gmail.com>
2024-11-21 22:36:50 +0700

    move database sql to repository and testing func userlogin

---
+++ repository/repositoryMock.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+type RepoMock struct {
+	mock.Mock
+}
+
+// AddProduct implements Repository.
+func (r *RepoMock) AddProduct(productName string, price float64, stock int) error {
+	panic("unimplemented")
+}
+
+// CurrentStockReport implements Repository.
+func (r *RepoMock) CurrentStockReport() error {
+	panic("unimplemented")
+}
+
+// CustomersTransactionsReport implements Repository.
+func (r *RepoMock) CustomersTransactionsReport() error {
+	panic("unimplemented")
+}
+
+// DeleteProduct implements Repository.
+func (r *RepoMock) DeleteProduct(productName string) error {
+	panic("unimplemented")
+}
+
+// ShowAllProducts implements Repository.
+func (r *RepoMock) ShowAllProducts() error {
+	panic("unimplemented")
+}
+
+// TotalRevenueReport implements Repository.
+func (r *RepoMock) TotalRevenueReport() error {
+	panic("unimplemented")
+}
+
+// UpdateProduct implements Repository.
+func (r *RepoMock) UpdateProduct(productId int, productName string, price float64, stock int) error {
+	panic("unimplemented")
+}
+
+func (r *RepoMock) UserLogin(email, password string) (string, error) {
+	args := r.Called(email, password)
+	return args.String(0), args.Error(1)
+}
