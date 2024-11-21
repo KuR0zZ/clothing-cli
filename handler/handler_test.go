52a1099cc152567a696eeaed6cc44db0707ac9f2
Dominus39 <yugoalexander@gmail.com>
2024-11-22 05:44:07 +0700

    make testAddProduct and fix addProduct

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"clothing-cli/repository"
+	"errors"
 	"fmt"
 	"testing"
 
@@ -16,8 +17,13 @@ type UserLoginTest struct {
 	email    string
 	password string
 }
+type AddProductTest struct {
+	name  string
+	price float64
+	stock int
+}
 
-func testUserLogin(t *testing.T) {
+func TestUserLogin(t *testing.T) {
 	user := UserLoginTest{
 		email:    "santoso@gmail.com",
 		password: "santoso123",
@@ -44,4 +50,45 @@ func TestUserLoginFail(t *testing.T) {
 	expectedError := fmt.Sprintf("incorrect password for email %s", user.email)
 
 	assert.Equal(t, expectedError, result.Error())
+}
+
+func TestAddProduct(t *testing.T) {
+	// Arrange
+	product := AddProductTest{
+		name:  "TestProduct",
+		price: 99999,
+		stock: 10,
+	}
+
+	// Mock the repository method
+	repoMock.On("AddProduct", product.name, product.price, product.stock).Return(nil)
+
+	// Act
+	err := userHandler.AddProduct(product.name, product.price, product.stock)
+
+	// Assert
+	assert.Nil(t, err)                                                                 // Ensure no error occurred
+	repoMock.AssertCalled(t, "AddProduct", product.name, product.price, product.stock) // Ensure method was called
+}
+
+func TestAddProductFail(t *testing.T) {
+	// Arrange
+	product := AddProductTest{
+		name:  "TestProduct",
+		price: 99999,
+		stock: 10,
+	}
+
+	expectedErr := errors.New("database error")
+
+	// Mock the repository method to return an error
+	repoMock.On("AddProduct", product.name, product.price, product.stock).Return(expectedErr)
+
+	// Act
+	err := userHandler.AddProduct(product.name, product.price, product.stock)
+
+	// Assert
+	assert.NotNil(t, err)                                                              // Ensure an error occurred
+	assert.Equal(t, expectedErr, err)                                                  // Ensure the returned error matches the mock
+	repoMock.AssertCalled(t, "AddProduct", product.name, product.price, product.stock) // Ensure method was called
 }
